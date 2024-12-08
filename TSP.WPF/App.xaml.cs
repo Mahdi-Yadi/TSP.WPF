ec7d51a514d492a42ada87a2ea72d3b12618a43e
Mahdi-Yadi <137503197+Mahdi-Yadi@users.noreply.github.com>
2024-12-08 17:32:53 +0330

    Add project files.

---
+++ TSP.WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TSP.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
