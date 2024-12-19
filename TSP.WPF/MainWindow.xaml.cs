949150e4d837e002949e89aa32bd0a9ba5aa31a6
Mahdi <137503197+Mahdi-Yadi@users.noreply.github.com>
2024-12-19 11:55:01 +0330

    add

--- TSP.WPF/MainWindow.xaml.cs
+++ TSP.WPF/MainWindow.xaml.cs
@@ -1,12 +1,19 @@
 ﻿using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
+
 namespace TSP.WPF;
 public partial class MainWindow : Window
 {
+
+    private List<Point> cities = new List<Point>();
+
     public MainWindow()
     {
         InitializeComponent();
     }
+
+    #region BTNs
 
     private void appdrag_OnMouseDown(object sender, MouseButtonEventArgs e)
     {
@@ -16,7 +23,6 @@ public partial class MainWindow : Window
         }
     }
 
-    
     private void close_OnMouseDown(object sender, MouseButtonEventArgs e)
     {
         Application.Current.Shutdown();
@@ -27,10 +33,9 @@ public partial class MainWindow : Window
         WindowState = WindowState.Minimized;
     }
 
-    private void CityInput_OnKeyDown(object sender, KeyEventArgs e)
-    {
-        throw new NotImplementedException();
-    }
+    #endregion
+
+    #region Algo
 
     // الگوریتم حریصانه 1
     private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
@@ -43,9 +48,56 @@ public partial class MainWindow : Window
         throw new NotImplementedException();
     }
 
+    #endregion
+
+    #region Other
+
+    private void CityInput_OnKeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            AddCity_Click(sender,e);
+        }
+    }
+
     private void AddCity_Click(object sender, RoutedEventArgs e)
     {
-        throw new NotImplementedException();
+        try
+        {
+
+            string[] ci = cityInput.Text.Split(',');
+
+            if (ci.Length == 2)
+            {
+
+                double x = double.Parse(ci[0]);
+                double y = double.Parse(ci[1]);
+
+                if (x < 0 || y < 0 || x > CityCanvas.ActualWidth || y > CityCanvas.ActualHeight)
+                {
+                    MessageBox.Show("مختصات وارد شده خارج از حد مرزی است!");
+                    return;
+                }
+
+                cities.Add(new Point(x,y));
+                CityList.Items.Add($"({x},{y})");
+                cityInput.Clear();
+
+                // نمایش شهر
+                //DrawCity(x,y);
+
+            }
+            else
+            {
+                MessageBox.Show("محور x و محور y را به درستی وارد نمایید.");
+            }
+
+        }
+        catch (Exception)
+        {
+            MessageBox.Show("فرمت وارد شده درست نمی باشد!");
+            throw;
+        }
     }
 
     private void clear_OnClick(object sender, RoutedEventArgs e)
@@ -57,5 +109,7 @@ public partial class MainWindow : Window
     {
         throw new NotImplementedException();
     }
+
+    #endregion
 
 }
